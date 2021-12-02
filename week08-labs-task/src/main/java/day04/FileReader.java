6a67dfa93c43f4c4b40772cce69da7b339f11f89
Attila <attila.ferenc.80@gmail.com>
2021-12-02 21:50:23 +0100

    day04 FileReader ugly solution solved

--- week08-labs-task/src/main/java/day04/FileReader.java
+++ week08-labs-task/src/main/java/day04/FileReader.java
@@ -2,7 +2,7 @@ package day04;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,21 +10,19 @@ public class FileReader {
 
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-        System.out.println(fileReader.findSmallestTemperatureSpread(Path.of("src/main/resources/weather.dat")));
-        System.out.println(fileReader.findSmallestDifference(Path.of("src/main/resources/football.dat")));
+        System.out.println(fileReader.findSmallestTemperatureSpread("weather.dat"));
+        System.out.println(fileReader.findSmallestDifference("football.dat"));
     }
 
-    public String findSmallestDifference(Path path) {
-        List<String> contentOfFile = readFile(path);
-        List<Integer> differences = getDifferences(contentOfFile);
-        int index = getDayWithSmallestSpread(differences);
-        return contentOfFile.get(index+1).substring(7,23).trim();
-    }
-
-    public int findSmallestTemperatureSpread(Path path) {
-        List<String> contentOfFile = readFile(path);
+    public int findSmallestTemperatureSpread(String filename) {
+        List<String> contentOfFile = readFile(filename);
         List<Integer> spreads = getSpreads(contentOfFile);
         return getDayWithSmallestSpread(spreads) + 1;
+    }
+
+    public String findSmallestDifference(String filename) {
+        List<String> contentOfFile = readFile(filename);
+        return getTeam (contentOfFile);
     }
 
     private int getDayWithSmallestSpread(List<Integer> spreads) {
@@ -51,25 +49,34 @@ public class FileReader {
         return spreads;
     }
 
-    private List<Integer> getDifferences(List<String> contentOfFile) {
-        int givenGoals;
-        int receivedGoals;
+    private String getTeam(List<String> contentOfFile) {
+        int givenGoal;
+        int receivedGoal;
+        List<String> teams = new ArrayList<>();
         List<Integer> differences = new ArrayList<>();
-        for (int i = 1; i < contentOfFile.size() - 1; i++) {
-            try {
-            givenGoals = (Integer.parseInt(contentOfFile.get(i).substring(43, 45).trim()));
-            receivedGoals = (Integer.parseInt(contentOfFile.get(i).substring(50, 52).trim()));
-            differences.add(givenGoals - receivedGoals);
-            } catch (NumberFormatException nfe) {
+        for (int i = 1; i < contentOfFile.size(); i++) {
+            if (i == 18) {
                 continue;
             }
+            givenGoal = (Integer.parseInt(contentOfFile.get(i).substring(43, 45)));
+            receivedGoal = (Integer.parseInt(contentOfFile.get(i).substring(50, 52)));
+            teams.add(contentOfFile.get(i).substring(7, 23).trim());
+            differences.add(givenGoal - receivedGoal);
         }
-        return differences;
+        int minDifference = Integer.MAX_VALUE;
+        int teamIndex = -1;
+        for (int i = 0; i < differences.size(); i++) {
+            if (differences.get(i) < minDifference) {
+                minDifference = differences.get(i);
+                teamIndex = i;
+            }
+        }
+        return teams.get(teamIndex);
     }
 
-    private List<String> readFile(Path path) {
+    private List<String> readFile(String filename) {
         try {
-            return Files.readAllLines(path);
+            return Files.readAllLines(Paths.get("src/main/resources/" + filename));
         } catch (IOException ioe) {
             throw new IllegalArgumentException("Unable to  read file", ioe);
         }
