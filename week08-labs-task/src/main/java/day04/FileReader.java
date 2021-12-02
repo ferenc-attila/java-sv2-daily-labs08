2c974bad5b9c5cd91cbea361f30af46f134a1c58
Attila <attila.ferenc.80@gmail.com>
2021-12-02 17:19:01 +0100

    day04 FileReader football wrong solution

--- week08-labs-task/src/main/java/day04/FileReader.java
+++ week08-labs-task/src/main/java/day04/FileReader.java
@@ -10,14 +10,21 @@ public class FileReader {
 
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-
         System.out.println(fileReader.findSmallestTemperatureSpread(Path.of("src/main/resources/weather.dat")));
+        System.out.println(fileReader.findSmallestDifference(Path.of("src/main/resources/football.dat")));
+    }
+
+    public String findSmallestDifference(Path path) {
+        List<String> contentOfFile = readFile(path);
+        List<Integer> differences = getDifferences(contentOfFile);
+        int index = getDayWithSmallestSpread(differences);
+        return contentOfFile.get(index+1).substring(7,23).trim();
     }
 
     public int findSmallestTemperatureSpread(Path path) {
         List<String> contentOfFile = readFile(path);
         List<Integer> spreads = getSpreads(contentOfFile);
-        return getDayWithSmallestSpread(spreads);
+        return getDayWithSmallestSpread(spreads) + 1;
     }
 
     private int getDayWithSmallestSpread(List<Integer> spreads) {
@@ -26,7 +33,7 @@ public class FileReader {
         for (int i = 0; i < spreads.size(); i++) {
             if (spreads.get(i) < spread) {
                 spread = spreads.get(i);
-                day = i + 1;
+                day = i;
             }
         }
         return day;
@@ -36,12 +43,28 @@ public class FileReader {
         int minTemp;
         int maxTemp;
         List<Integer> spreads = new ArrayList<>();
-        for (int i = 2; i < contentOfFile.size() - 2; i++) {
+        for (int i = 2; i < contentOfFile.size() - 1; i++) {
             maxTemp = (Integer.parseInt(contentOfFile.get(i).substring(6, 8).trim()));
             minTemp = (Integer.parseInt(contentOfFile.get(i).substring(12, 14).trim()));
             spreads.add(maxTemp - minTemp);
         }
         return spreads;
+    }
+
+    private List<Integer> getDifferences(List<String> contentOfFile) {
+        int givenGoals;
+        int receivedGoals;
+        List<Integer> differences = new ArrayList<>();
+        for (int i = 1; i < contentOfFile.size() - 1; i++) {
+            try {
+            givenGoals = (Integer.parseInt(contentOfFile.get(i).substring(43, 45).trim()));
+            receivedGoals = (Integer.parseInt(contentOfFile.get(i).substring(50, 52).trim()));
+            differences.add(givenGoals - receivedGoals);
+            } catch (NumberFormatException nfe) {
+                continue;
+            }
+        }
+        return differences;
     }
 
     private List<String> readFile(Path path) {
